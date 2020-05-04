15c273fbb3fef635ab333c130bea2e7da13b7699
ldkhoi100 <ldkhoi100@gmail.com>
2020-05-04 17:27:47 +0700

    ud

--- database/seeds/LinhVucTableSeeder.php
+++ database/seeds/LinhVucTableSeeder.php
@@ -12,41 +12,35 @@ class LinhVucTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('linh_vuc')->delete();
-        
-        \DB::table('linh_vuc')->insert(array (
-            0 => 
-            array (
-                'id' => 1,
+
+        \DB::table('linh_vuc')->insert(array(
+            0 =>
+            array(
                 'Ten_LV' => 'Giám Đốc',
                 'created_at' => NULL,
                 'updated_at' => NULL,
             ),
-            1 => 
-            array (
-                'id' => 2,
+            1 =>
+            array(
                 'Ten_LV' => 'Quản Lý',
                 'created_at' => NULL,
                 'updated_at' => NULL,
             ),
-            2 => 
-            array (
-                'id' => 3,
+            2 =>
+            array(
                 'Ten_LV' => 'Pha Chế',
                 'created_at' => NULL,
                 'updated_at' => NULL,
             ),
-            3 => 
-            array (
-                'id' => 4,
+            3 =>
+            array(
                 'Ten_LV' => 'Phục Vụ',
                 'created_at' => NULL,
                 'updated_at' => NULL,
             ),
         ));
-        
-        
     }
 }
