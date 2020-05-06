db6391d9db06b4bb222756e681a38692b0e8f072
Vinh Hiển <hienvh00@gmail.com>
2020-05-06 23:21:47 +0700

    Update semifinal crud table bacluong by Hien

--- public/js/factor_salary.js
+++ public/js/factor_salary.js
@@ -1,93 +1,94 @@
-$(document).ready(function () {
-    $('.btn-create').click(function () {
-        let url = $(this).data('url');
+let FS = {} || FS;
 
-        $('.modal-body').load(url, function () {
-            $('.btn-submit').click(function (e) {
-                e.preventDefault();
-                let url = $(this).data('url');
-                let data = $(this).parent('form').serialize();
-
-                $.post(url, data).done(function (data) {
-                    if (!data) {
-                        $('.modal').modal('hide');
-                        loadTable();
-                    } else {
-                        $.each(data, function (k, v) {
-                            $(`input[name=${k}]`).addClass('is-invalid');
-                            $(`input[name=${k}]`).parent('div').find('small').attr('class', 'alert alert-warning p-0').html(v);
-                        });
-                    }
-                });
-            });
-        });
-
-        $('.modal').modal('show');
-    });
-
-    $('.allfs').load('/factor-salary/index', function () {
-        $('.btn-show').click(function (e) {
-            e.preventDefault();
-            let url = $(this).data('url');
-            $.get(url, function (data) {
-
-            });
-        });
-
-        $('.btn-edit').click(function (e) {
-            e.preventDefault();
-            let url = $(this).data('url');
-            $('.modal-body').load(url, function () {
-
-                $('.btn-submit').click(function (e) {
-                    e.preventDefault();
-                    let url = $(this).data('url');
-                    let data = $(this).parent('form');
-                    $.ajax({
-                            url: url,
-                            method: 'patch',
-                            data: {
-                                "_token": data.find('input[name=_token]').val(),
-                                "Bac_Luong": data.find('input[name=Bac_Luong]').val()
-                            }
-                        })
-                        .done(function (data) {
-                            if (data) {
-                                $.each(data, function (k, v) {
-                                    $(`input[name=${k}]`).addClass('is-invalid');
-                                    $(`input[name=${k}]`).parent('div').find('small').attr('class', 'alert alert-warning p-0').html(v);
-                                });
-                            } else {
-                                $('.modal').modal('hide');
-                                loadTable();
-                            }
-                        });
-                });
-            });
-
-            $('.modal').modal('show');
-
-        });
-
-    });
-});
-
-function loadTable() {
-    $('.allfs').load('/factor-salary/index', function () {
-        $('.btn-show').click(function (e) {
-            e.preventDefault();
-            let url = $(this).data('url');
-            $.get(url, function (data) {
-
-            });
-        });
-
-        $('.btn-edit').click(function (e) {
-            e.preventDefault();
-            let url = $(this).data('url');
-            $('.modal-body').load(url);
-            $('.modal').modal('show');
-        });
-
+FS.RenderData = function () {
+    $.get('/factor-salary').done(function (data) {
+        $('.render-data').html(data);
     });
 }
+
+FS.Create = function () {
+    $.get('/factor-salary/create').done(function (data) {
+        $('.crud-fator-salary').html(data).find('.modal').modal('show');
+    });
+}
+
+FS.Edit = function (btn) {
+    let url = $(btn).data('url');
+    $.get(url).done(function (data) {
+        $('.crud-fator-salary').html(data).find('.modal').modal('show');
+    });
+}
+
+// FS.Show = function (btn) {
+//     let url = $(btn).data('url');
+//     $.get(url).done(function (data) {
+//         $('.crud-fator-salary').html(data).find('.modal').modal('show');
+//     });
+// }
+
+FS.Delete = function (btn) {
+    let url = $(btn).data('url');
+    $.ajax({
+        url: url,
+        method: 'delete'
+    }).done(function (data) {
+        FS.Noti(data);
+    });
+}
+
+FS.Store = function (form) {
+    let data = $(form).serialize();
+    $.post('/factor-salary', data).done(function (data) {
+        if (data) {
+            $.each(data, function (k, v) {
+                FS.Errors(k, v);
+            });
+        } else {
+            FS.Noti('Create success');
+        }
+    });
+}
+
+FS.Update = function (btn) {
+    let url = $(btn).data('url');
+    let data = $(btn.form).serialize();
+    $.ajax({
+        url: url,
+        method: 'put',
+        data: data
+    }).done(function (data) {
+        if (data) {
+            $.each(data, function (k, v) {
+                FS.Errors(k, v);
+            });
+        } else {
+            FS.Noti('Update Success');
+        }
+    });
+}
+
+FS.Noti = function (msg) {
+    FS.RenderData();
+    $('.modal').modal('hide')
+    $('.container').prepend($('<div>').addClass('alert alert-success').text(msg));
+    setTimeout(function () {
+        $('.alert').remove();
+    }, 5000);
+}
+
+FS.Errors = function (k, v) {
+    $(`input[name=${k}]`).addClass('is-invalid').data('content', v[0]).data('placement', 'top').data('toggle', 'popover').popover("show");
+    $('.crud-fator-salary').change(function () {
+        $('.is-invalid').removeClass('is-invalid').removeData('placement').removeData('toggle').removeData('content');
+    });
+}
+
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
+$(document).ready(function () {
+    FS.RenderData();
+});
