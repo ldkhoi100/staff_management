358780066ecf0127208ce42ccf17b92d7d6b40e8
vansang1201200 <levansanglvs9@gmail.com>
2020-05-06 11:08:16 +0700

    sang

---
+++ app/Repositories/Impl/RoleRepositoryImpl.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Repositories\Impl;
+
+use App\Model\Role;
+use App\Repositories\RoleRepository;
+use App\Repositories\Eloquent\EloquentRepository;
+
+class RoleRepositoryImpl extends EloquentRepository implements RoleRepository
+{
+    public function getModel()
+    {
+       $model = Role::class;
+       return $model;
+    }
+
+    public function findOnlyTrashed($id)
+    {
+        $result = $this->model->onlyTrashed($id)->where('id', $id)->first();
+        return $result;
+    }
+}
+
