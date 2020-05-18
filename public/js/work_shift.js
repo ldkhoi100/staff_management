b21c1f5ce795545c4ed8cb77a5eb6533b6841e90
ldkhoi100 <ldkhoi100@gmail.com>
2020-05-18 08:38:46 +0700

    ud

--- public/js/work_shift.js
+++ public/js/work_shift.js
@@ -3,17 +3,17 @@ let Ws = {} || Ws;
 Ws.table;
 Ws.tableTrash;
 
-Ws.drawTable = function () {
+Ws.drawTable = function() {
     Ws.table = $('#ws-table').DataTable({
         processing: true,
         ajax: {
             url: '/work-shift/all',
-            dataSrc: function (jsons) {
+            dataSrc: function(jsons) {
                 return jsons.map(json => {
                     return {
                         ca: json.Ca,
                         hs: json.He_So,
-                        crt: json.created_at.split(' ', 1)[0],
+                        crt: json.created_at,
                         action: `
                             <a class="btn btn-secondary text-light" onclick="Ws.edit(${json.id})">Edit</a>
                             <a class="btn btn-warning text-dark" onclick="Ws.trash(${json.id})">Trash</a>
@@ -39,17 +39,17 @@ Ws.drawTable = function () {
     });
 };
 
-Ws.drawTableTrash = function () {
+Ws.drawTableTrash = function() {
     Ws.tableTrash = $('#ws-table-trash').DataTable({
         processing: true,
         ajax: {
             url: '/work-shift/trash',
-            dataSrc: function (jsons) {
+            dataSrc: function(jsons) {
                 return jsons.map(json => {
                     return {
                         ca: json.Ca,
                         hs: json.He_So,
-                        dlt: json.deleted_at.split(' ', 1)[0],
+                        dlt: json.deleted_at,
                         action: `
                             <a class="btn btn-primary text-light" onclick="Ws.undo(${json.id})">Undo</a>
                             <a class="btn btn-danger text-light" onclick="Ws.delete(${json.id})">Delete</a>
@@ -75,17 +75,17 @@ Ws.drawTableTrash = function () {
     });
 };
 
-Ws.trash = function (id) {
+Ws.trash = function(id) {
     if (confirm('Move this to Trash')) {
         $.ajax({
             url: `/work-shift/${id}`,
             method: "delete",
-            success: function (msg) {
+            success: function(msg) {
                 Ws.success(msg);
                 Ws.table.ajax.reload();
                 Ws.tableTrash.ajax.reload();
             },
-            error: function (errors) {
+            error: function(errors) {
                 Ws.errors(errors);
             }
         });
@@ -93,19 +93,19 @@ Ws.trash = function (id) {
 }
 
 
-Ws.edit = function (id) {
-    $.get(`/work-shift/${id}`).done(function (Obj) {
+Ws.edit = function(id) {
+    $.get(`/work-shift/${id}`).done(function(Obj) {
         $.each(Obj, (i, v) => {
             $(`#ws-modal [name=${i}]`).val(v);
         });
         $('#ws-modal #btn-save').data('id', Obj.id);
         Ws.openModal(1);
-    }).fail(function (errors) {
+    }).fail(function(errors) {
         Ws.errors(errors);
     });
 }
 
-Ws.openModal = function (edit = null) {
+Ws.openModal = function(edit = null) {
     if (edit) {
         $('#ws-modal #ws-modal-title').text("Edit Work Shift");
     } else {
@@ -119,60 +119,58 @@ Ws.openModal = function (edit = null) {
     $('#ws-modal').modal('show');
 }
 
-Ws.create = function () {
+Ws.create = function() {
     Ws.openModal();
 }
 
-Ws.undo = function (id) {
+Ws.undo = function(id) {
     if (confirm("Undo this")) {
         $.ajax({
             url: `/work-shift/${id}/restore`,
             method: 'PUT',
-            success: function (msg) {
+            success: function(msg) {
                 Ws.success(msg);
                 Ws.tableTrash.ajax.reload();
                 Ws.table.ajax.reload();
             },
-            error: function (errors) {
+            error: function(errors) {
                 Ws.errors(errors);
             }
         });
     }
 }
 
-Ws.delete = function (id) {
+Ws.delete = function(id) {
     if (confirm('Delete this')) {
         $.ajax({
             url: `/work-shift/${id}/delete`,
             method: 'delete',
-            success: function (msg) {
+            success: function(msg) {
                 Ws.success(msg);
                 Ws.tableTrash.ajax.reload();
             },
-            error: function (errors) {
+            error: function(errors) {
                 Ws.errors(errors);
             }
         });
     }
 }
 
-Ws.save = function (btn) {
+Ws.save = function(btn) {
     let id = $(btn).data('id');
     let data = $(btn.form).serializeJSON();
-    console.log(id);
-    console.log(data);
     if (id) {
         if (confirm('Save change')) {
             $.ajax({
                 url: `/work-shift/${id}`,
                 method: 'PUT',
                 data: data,
-                success: function (Obj) {
+                success: function(Obj) {
                     Ws.table.ajax.reload();
                     $('#ws-modal').modal("hide");
                     Ws.success("Update success!");
                 },
-                error: function (errors) {
+                error: function(errors) {
                     Ws.errors(errors);
                 }
             });
@@ -183,12 +181,12 @@ Ws.save = function (btn) {
                 url: `/work-shift`,
                 method: 'post',
                 data: data,
-                success: function (data) {
+                success: function(data) {
                     Ws.table.ajax.reload();
                     $('#ws-modal').modal("hide");
                     Ws.success("Create success");
                 },
-                error: function (errors) {
+                error: function(errors) {
                     Ws.errors(errors);
                 }
             });
@@ -196,7 +194,7 @@ Ws.save = function (btn) {
     }
 }
 
-Ws.success = function (msg, status = "Success", icon = "success") {
+Ws.success = function(msg, status = "Success", icon = "success") {
     $.toast({
         heading: status,
         text: msg,
@@ -207,14 +205,14 @@ Ws.success = function (msg, status = "Success", icon = "success") {
     });
 }
 
-Ws.errors = function (errors) {
+Ws.errors = function(errors) {
     if (errors.status == 422) {
         let msg = errors.responseJSON.errors;
         $(`#ws-modal .field`).removeClass('is-invalid');
         $(`#ws-modal .field`).removeClass('is-valid');
         $(`#ws-modal .field`).addClass('is-valid');
         $('small.text').remove();
-        $.each(msg, function (i, v) {
+        $.each(msg, function(i, v) {
             $(`#ws-modal [name=${i}]`).addClass('is-invalid').after(`<small class="text text-danger mx-auto">${v}</small>`);
         });
     } else {
@@ -223,12 +221,12 @@ Ws.errors = function (errors) {
     }
 }
 
-Ws.init = function () {
+Ws.init = function() {
     Ws.drawTable();
     Ws.drawTableTrash();
 }
 
-$(document).ready(function () {
+$(document).ready(function() {
     Ws.init();
     $.ajaxSetup({
         headers: {
