9e882f9c6cef17e197eaf0c4931d137a538c9f9f
ldkhoi100 <ldkhoi100@gmail.com>
2020-05-21 08:52:55 +0700

    ud

--- database/migrations/2020_05_20_154837_create_cham_cong_ngay_table.php
+++ database/migrations/2020_05_20_154837_create_cham_cong_ngay_table.php
@@ -3,7 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateChamCongNgayTable extends Migration {
+class CreateChamCongNgayTable extends Migration
+{
 
 	/**
 	 * Run the migrations.
@@ -12,18 +13,13 @@ class CreateChamCongNgayTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('cham_cong_ngay', function(Blueprint $table)
-		{
+		Schema::create('cham_cong_ngay', function (Blueprint $table) {
 			$table->increments('id');
 			$table->integer('MaNV')->unsigned()->index('MaNV');
 			$table->integer('LuongCB')->unsigned()->index('LuongCB');
 			$table->date('Ngay_Hien_Tai')->nullable();
 			$table->boolean('Ngay_Le')->default(0);
-<<<<<<< HEAD:database/migrations/2020_05_20_154837_create_cham_cong_ngay_table.php
 			$table->integer('Luong')->default(100);
-=======
-			$table->boolean('Luong')->default(0);
->>>>>>> 94fee29561e4bcdebd2199b811696f59009bd171:database/migrations/2020_05_19_163040_create_cham_cong_ngay_table.php
 			$table->text('Ghi_Chu', 65535)->nullable();
 			$table->timestamps();
 			$table->softDeletes();
@@ -40,5 +36,4 @@ class CreateChamCongNgayTable extends Migration {
 	{
 		Schema::drop('cham_cong_ngay');
 	}
-
 }
