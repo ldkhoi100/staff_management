0ed80b6a6178e5e791f5277917074f009f37aef2
ldkhoi100 <ldkhoi100@gmail.com>
2020-05-27 11:00:22 +0700

    ud

--- resources/views/timesheets/index.blade.php
+++ resources/views/timesheets/index.blade.php
@@ -15,76 +15,74 @@
 </style>
 
 <div class="container-fluid my-5">
-    <div class="container">
-        <nav class="nav">
-            <li class="nav-item">
-                <div class="form-group">
-                    <div>
-                        <div class="form-group">
-                            <label>Base Salary</label>
-                            <select id="baseSalary" class="form-control"
-                                onchange="Ts.updateBaseSalary(this.value)"></select>
-                        </div>
-
+    <nav class="nav">
+        <li class="nav-item">
+            <div class="form-group">
+                <div>
+                    <div class="form-group">
+                        <label>Base Salary</label>
+                        <select id="baseSalary" class="form-control"
+                            onchange="Ts.updateBaseSalary(this.value)"></select>
                     </div>
 
                 </div>
-            </li>
 
-            <li class="nav-item mx-auto">
-                <button type="button" class="btn btn-info" onclick="Ts.statistic()">TimeSheets Statistic</button>
-            </li>
+            </div>
+        </li>
 
-            <li class="nav-item">
-                <div class="custom-control custom-switch">
-                    <input type="checkbox" class="custom-control-input" id="holiday">
-                    <label class="custom-control-label" for="holiday">Holiday</label>
-                </div>
-            </li>
-        </nav>
-    </div>
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">
-                <span style="float: left">Manager Timesheets<br>
-                    Your role:<span style="color: brown">
-                        @if(count(Auth::user()->roles) > 0)
-                        {{ Auth::user()->roles[0]->name }}
-                        @endif
-                    </span>
+        <li class="nav-item mx-auto">
+            <button type="button" class="btn btn-info" onclick="Ts.statistic()">TimeSheets Statistic</button>
+        </li>
+
+        <li class="nav-item">
+            <div class="custom-control custom-switch">
+                <input type="checkbox" class="custom-control-input" id="holiday">
+                <label class="custom-control-label" for="holiday">Holiday</label>
+            </div>
+        </li>
+    </nav>
+</div>
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">
+            <span style="float: left">Manager Timesheets<br>
+                Your role:<span style="color: brown">
+                    @if(count(Auth::user()->roles) > 0)
+                    {{ Auth::user()->roles[0]->name }}
+                    @endif
                 </span>
-                <div class="form-group" style="float: right">
-                    <label> Current Day</label>
-                    <input type="date" class="form-control" value="{{ date('Y-m-d') }}" id="current-day">
-                </div>
-            </h6>
+            </span>
+            <div class="form-group" style="float: right">
+                <label> Current Day</label>
+                <input type="date" class="form-control" value="{{ date('Y-m-d') }}" id="current-day">
+            </div>
+        </h6>
 
-        </div>
+    </div>
 
-        <div class="tab-content">
-            <div class="tab-pane active" id="home">
-                <div class="card-body" id="reload_table">
-                    <table id="bang-chamcong" class="table table-striped mt-2" style="text-align: center">
-                        <thead class="thead-dark">
-                            <tr>
-                                <th width="1%">#</th>
-                                <th>Full Name</th>
-                                <th>Position</th>
-                                <th>Work Shift</th>
-                                <th>Sabbatical Leave</th>
-                                <th>Salary</th>
-                                <th>Description</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        </tbody>
-                    </table>
-                </div>
+    <div class="tab-content">
+        <div class="tab-pane active" id="home">
+            <div class="card-body" id="reload_table">
+                <table id="bang-chamcong" class="table table-striped mt-2" style="text-align: center">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th width="1%">#</th>
+                            <th>Full Name</th>
+                            <th>Position</th>
+                            <th width="20%">Work Shift</th>
+                            <th>Sabbatical Leave</th>
+                            <th width="10%">Salary</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
-
 </div>
+
 <div id="them-mota" class="modal fade" tabindex="-1" role="dialog" data-backdrop="static">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -102,9 +100,9 @@
             </div>
         </div>
     </div>
-</div>
-<div class="d-none" id="data-export">
-    @include('timesheets.export.table')
+    <div class="d-none" id="data-export">
+        @include('timesheets.export.table')
+    </div>
 </div>
 @include('timesheets.modal.timesheet_statistic_modal')
 @endsection
